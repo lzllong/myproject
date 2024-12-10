d715912e45bd5067a495d7125ab23c42f68f925b
lzl <2122486992@qq.com>
2024-12-10 19:13:13 +0800

    create Sum.c

---
+++ Sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main() {
+	       int a,b,sum;
+	       printf("please input two values a and b:");
+	
+		       scanf("%d%d",&a,&b);
+	
+		       sum=a+b;
+	       printf("sum=%d\n",sum);
+	
+	}
